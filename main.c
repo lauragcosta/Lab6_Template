66bdbe92a299e45d81d9cfb1df12f7bb3ac33a41
lauragcosta <202100242@estudantes.ips.pt>
2022-04-27 13:18:06 +0100

    NIVEL 2 + 3 + 4

--- main.c
+++ main.c
@@ -10,8 +10,51 @@ int main() {
         return EXIT_FAILURE;
     }
 
+    //NIVEL 1
+    /*
     vector3dPrint(v1, true);
     free(v1);
-    
+    */
+
+    //NIVEL 2
+    PtVector3d v2 = NULL;
+    vector3dPrint(v1, true);
+    vector3dPrint(v2, true);
+
+    vector3dDestroy(&v1);
+    vector3dDestroy(&v2);
+
+    //NIVEL 3
+    /*
+    printComponentOf(vNull);
+    printComponentOf(v2);
+    */
+    //NIVEL 4
+    PtVector3d v1a = vector3dMultiplyByScalar(v1, 2.5);
+    vector3dPrint(v1a, true);
+
+    PtVector3d v3 = vector3dAdd(v1, v2);
+    vector3dPrint(v3, true);
+
+    double magnitude;
+    vector3dMagnitude(v1, &magnitude);
+    printf("Magnitude: %.2f\n", magnitude);
+
     return EXIT_SUCCESS;
+}
+
+//NIVEL 3
+void printComponentOf(PtVector3d vec)
+{
+    double x, y, z;
+    if(vector3dX(vec, &x) != VECTOR3D_OK)
+    {
+        printf("Vector NULL. Não é possível apresentar as componentes.\n");
+    }
+    else
+    {
+        vector3dY(vec, &y);
+        vector3dZ(vec, &z);
+        printf("x = %.2f | y = %.2f | z = %.2f\n", x, y, z);
+    }
 }
--- vector3d.c
+++ vector3d.c
@@ -40,41 +40,98 @@ void vector3dPrint(PtVector3d vec, bool lineBreakAfter)
 }
 
 //NIVEL 2
+int vector3dDestroy(PtVector3d *pVec)
+{
+    PtVector3d vec = *pVec;
+ 
+    if (vec == NULL)
+    {
+        return VECTOR3D_NULL;
+    }
+
+    free(vec);
+    pVec = NULL;
+
+    return VECTOR3D_OK;
+}
+
+//NIVEL 3
+int vector3dX(PtVector3d vec, double *x)
+{
+    if (vec == NULL) 
+    {
+        return VECTOR3D_NULL;
+    }
+
+    *x = vec -> x;
+
+    return VECTOR3D_OK;
+}
+
+int vector3dY(PtVector3d vec, double *y)
+{
+    if (vec == NULL) 
+    {
+        return VECTOR3D_NULL;
+    }
+
+    *y = vec -> y;
+
+    return VECTOR3D_OK;
+}
+
+int vector3dZ(PtVector3d vec, double *z)
+{
+    if (vec == NULL) 
+    {
+        return VECTOR3D_NULL;
+    }
+
+    *z = vec -> z;
+
+    return VECTOR3D_OK;
+}
+
+//NIVEL 4
+PtVector3d vector3dMultiplyByScalar(PtVector3d vec, double scalar)
+{
+    if(vec == NULL)
+    {
+        return NULL;
+    }
+
+    PtVector3d c = vector3dCreate(vec->x * scalar, vec->y * scalar, vec->z * scalar);
+
+    return c;
+}
+
+PtVector3d vector3dAdd(PtVector3d vec1, PtVector3d vec2)
+{
+    if(vec1 == NULL || vec2 == NULL)
+    {
+        return NULL;
+    }
+
+    PtVector3d c = vector3dCreate(vec1->x + vec2->x, vec1->y + vec2->y, vec1->z + vec2->z);
+
+    return c;
+}
+
+int vector3dMagnitude(PtVector3d vec, double *magnitude)
+{
+    if(vec == NULL)
+    {
+        return VECTOR3D_NULL;
+    }
+
+    *magnitude = sqrt(vec->x * vec->x + vec->y * vec->y + vec->z * vec->z);
+
+    return VECTOR3D_OK;
+}
 /*
-int vector3dX(PtVector3d vec, double *x) {
+PtVector3d vector3dNormalize(PtVector3d vec)
+{
     
-    return VECTOR3D_OK;
-}
-
-int vector3dY(PtVector3d vec, double *y) {
-    
-    return VECTOR3D_OK;
-}
-
-int vector3dZ(PtVector3d vec, double *z) {
-    
-    return VECTOR3D_OK;
-}
-
-int vector3dDestroy(PtVector3d *pVec) {
-    
-    return VECTOR3D_OK;
-}
-
-PtVector3d vector3dMultiplyByScalar(PtVector3d vec, double scalar) {
-    return NULL;
-}
-
-PtVector3d vector3dAdd(PtVector3d vec1, PtVector3d vec2) {
-    return NULL;
-}
-
-int vector3dMagnitude(PtVector3d vec, double *magnitude) {
-   
-    return VECTOR3D_OK;
-}
-
-PtVector3d vector3dNormalize(PtVector3d vec) {
-    return NULL;
 }
 */
+
